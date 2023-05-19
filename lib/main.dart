ce5ef49381779fe448c61753af58c0cf6a8420da
Nagi Mohamed <117310405+nagiElshershaby@users.noreply.github.com>
2023-05-19 06:37:21 +0300

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:developer' as developer show log;
 
-import 'views/home_page.dart';
+extension Log on Object{
+  void log() => developer.log(toString());
+}
 
 void main() {
   runApp(const MyApp());
@@ -20,5 +24,30 @@ class MyApp extends StatelessWidget {
       ),
       home: const HomePage(),
     );
+  }
+}
+
+void testIt() async {
+  final stream1 = Stream.periodic(
+    const Duration(seconds: 1),
+    (i) => 'Stream 1, count = $i',
+  );
+  final stream2 = Stream.periodic(
+    const Duration(seconds: 3),
+    (i) => 'Stream 2, count = $i',
+  );
+  final combined = Rx.combineLatest2(stream1, stream2, (one, two) => 'one is $one, two is $two',);
+  await for (final value in combined) {
+    value.log();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    testIt();
+    return const Placeholder();
   }
 }
